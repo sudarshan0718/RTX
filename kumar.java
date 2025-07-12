1e3eaff5df61d66704dc8b64aa8481ff7498375d
GitDemo <sudarshanchalaki@gmail.com>
2025-07-12 12:01:01 +0530

    hi

---
+++ kumar.java
@@ -0,0 +1,9 @@
+package RTX;
+public class kumar {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
